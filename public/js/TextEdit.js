b6284e56e9977170fcff1f5649ef8e7804564c10
TYLER DUNCAN <chico55_8008@yahoo.com>
2018-07-05 22:01:42 -0400

    javascript for the text editor

---
+++ public/js/TextEdit.js
@@ -0,0 +1,21 @@
+
+
+function compile()
+{
+     var code = document.getElementById("editor").value.toUpperCase();
+     console.log("Code: " + code);
+     console.log("Output: " + Lexer(code));
+}
+
+var textareas = document.getElementsByTagName('textarea');
+var count = textareas.length;
+for(var i=0;i<count;i++){
+    textareas[i].onkeydown = function(e){
+        if(e.keyCode==9 || e.which==9){
+            e.preventDefault();
+            var s = this.selectionStart;
+            this.value = this.value.substring(0,this.selectionStart) + "\t" + this.value.substring(this.selectionEnd);
+            this.selectionEnd = s+1;
+        }
+    }
+}
