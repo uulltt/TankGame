98ede9704c4e27ed9db4944c2b06fa77ebed1449
uulltt <ajvark@gmail.com>
2018-07-19 00:49:21 -0400

    stuff

--- server.js
+++ server.js
@@ -114,34 +114,34 @@ app.post('/login', function (req, response) {
 		checkInput(req.body)) {
 		response.status(400).send();
 	} else {
-		// let query = 'SELECT * FROM users WHERE username=\'' + req.body.username +'\';';
-		// client.query(query, (err, res) => {
-		// 	if (!err){
-		// 	if (res.rowCount == 1) {
-		// 		bcrypt.compare(req.body.password, res.rows[0].passwordhash, (err2, same) => {
-		// 			if (!err2){
-		// 			console.log("password compare: " + same);
-		// 			if (same) {
-		// 				req.session.loggedin = true;
-		// 				req.session.userid = res.rows[0].id;
-		// 				console.log("redirecting to dash");
-		// 				response.set('Access-Control-Allow-Origin','*');
-		// 				response.status(200).send();
-		// 			} else {
-		// 				response.status(401).send();
-		// 			}
-		// 			} else {
-		// 				console.log(err2);
-		// 			}
+		 let query = 'SELECT * FROM users WHERE username=\'' + req.body.username +'\';';
+		 client.query(query, (err, res) => {
+	 	if (!err){
+		 	if (res.rowCount == 1) {
+		 		bcrypt.compare(req.body.password, res.rows[0].passwordhash, (err2, same) => {
+		 			if (!err2){
+		 			console.log("password compare: " + same);
+		 			if (same) {
+		 				req.session.loggedin = true;
+		 				req.session.userid = res.rows[0].id;
+		 				console.log("redirecting to dash");
+		 				response.set('Access-Control-Allow-Origin','*');
+		 				response.status(200).send();
+		 			} else {
+					response.status(401).send();
+		 			}
+		 			} else {
+		 				console.log(err2);
+		 			}
 
-		// 		});
-		// 	} else {
-		// 		response.status(401).send();
-		// 	}
-		// 	} else {
-		// 		console.log(err);
-		// 	}
-		// });
+				});
+		 	} else {
+		 		response.status(401).send();
+		 	}
+		 	} else {
+		 		console.log(err);
+		 	}
+		 });
 	}
 });
 
