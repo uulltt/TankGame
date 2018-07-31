02e744ad60197cd2e6913624ca89ac9597d0acba
Tyler <neruelin@gmail.com>
2018-07-31 10:49:55 -0400

    adfs

--- public/js/Lexer.js
+++ public/js/Lexer.js
@@ -493,11 +493,11 @@ const turnState = (input, index) =>
      }
      else if(buf[1] === "RIGHT")
      {
-          return tokens.TURNSTATE + " " + tokens.RIGHTSYM + " " + tokens.NUMSYM + " " + buf[3] + " ";
+          return tokens.TURNSTATE + " " + tokens.RIGHTSYM + " " + tokens.NUMSYM + " " + buf[2] + " ";
      }
      else
      {
-          return tokens.TURNSTATE + " " + tokens.LEFTSYM + " " + tokens.NUMSYM + " " + buf[3] + " ";
+          return tokens.TURNSTATE + " " + tokens.LEFTSYM + " " + tokens.NUMSYM + " " + buf[2] + " ";
      }
 }
 
