0b79638695bb5671d0ab4b5d514d19a0e58ad119
Tyler <neruelin@gmail.com>
2018-07-31 04:24:17 -0400

    asdfasdfasdfasdfsdfasdfasdfasdf

--- public/js/MapJSONParser.js
+++ public/js/MapJSONParser.js
@@ -27,18 +27,38 @@ function loadMap(map)
           {
                var tC = map.layers[0].tiles[index].tile;
                console.log(tC)
-               if(tC == 8)
-               {
-                    new Terrain(map.layers[0].tiles[index].x, map.layers[0].tiles[index].y, "tile" + map.layers[0].tiles[index].tile + ".bmp")
+               switch (tC) {
+                    case 8:
+                         new Terrain(map.layers[0].tiles[index].x, map.layers[0].tiles[index].y, "tile" + map.layers[0].tiles[index].tile + ".bmp");
+                         break;
+                    case 0:
+                    case 1:
+                    case 2:
+                    case 3:
+                    case 4:
+                    case 5:
+                    case 6:
+                    case 7:
+                    case 13:
+                    case 14:
+                         new Barrier(map.layers[0].tiles[index].x, map.layers[0].tiles[index].y, "tile" + map.layers[0].tiles[index].tile + ".bmp");
+                         break;
+                    default:
+                         new Obstacle(map.layers[0].tiles[index].x, map.layers[0].tiles[index].y, "Obstacle", "tile" + map.layers[0].tiles[index].tile + ".bmp");
+                         break;
                }
-               else if(tC == 0 || tC == 1 || tC == 2 || tC == 3 || tC == 4 || tC == 5 || tC == 6 || tC == 7 || tC == 13 || tC == 14)
-               {
-                    new Barrier(map.layers[0].tiles[index].x, map.layers[0].tiles[index].y, "tile" + map.layers[0].tiles[index].tile + ".bmp");
-               }
-               else
-               {    
-                    new Obstacle(map.layers[0].tiles[index].x, map.layers[0].tiles[index].y, "Obstacle", "tile" + map.layers[0].tiles[index].tile + ".bmp");
-               }
+               // if(tC == 8)
+               // {
+               //      new Terrain(map.layers[0].tiles[index].x, map.layers[0].tiles[index].y, "tile" + map.layers[0].tiles[index].tile + ".bmp")
+               // }
+               // else if(tC == 0 || tC == 1 || tC == 2 || tC == 3 || tC == 4 || tC == 5 || tC == 6 || tC == 7 || tC == 13 || tC == 14)
+               // {
+               //      new Barrier(map.layers[0].tiles[index].x, map.layers[0].tiles[index].y, "tile" + map.layers[0].tiles[index].tile + ".bmp");
+               // }
+               // else
+               // {    
+               //      new Obstacle(map.layers[0].tiles[index].x, map.layers[0].tiles[index].y, "Obstacle", "tile" + map.layers[0].tiles[index].tile + ".bmp");
+               // }
                index++;
 
           }
