983efac868154cb24e3ae4f0175b78fd2282ff5c
Tduncan13 <chico55_8008@yahoo.com>
2018-07-31 04:08:56 -0400

    hmmm

--- public/js/MapJSONParser.js
+++ public/js/MapJSONParser.js
@@ -17,22 +17,19 @@ function loadMap(map)
 {
      console.log(map.tileshigh);
      var newMap = new Map(map.tileshigh, map.tileshigh);
-     SG.Board = newMap;
      var mapData = new MapArr(map.tileshigh, map.tileshigh);
      var index = 0;
 
-     for(var i = 0; i < mapData.rows; i++)
+     for(var i = 0; i < map.tileshigh; i++)
      {
-          for(var j = 0; j < mapData.columns; j++)
+          for(var j = 0; j < map.tileshigh; j++)
           {
-               var tC = map.layers[0].tiles[index].tile;
                console.log(map.layers[0].tiles[index].tile);
-               console.log((map.layers[0].tiles[index].tile >= 0 && map.layers[0].tiles[index].tile <= 7) || map.layers[0].tiles[index].tile == 13 || map.layers[0].tiles[index].tile == 14);
-               if(tC == 8)
+               if(map.layers[0].tiles[index].tile == 8)
                {
                     new Terrain(map.layers[0].tiles[index].x, map.layers[0].tiles[index].y, "tile" + map.layers[0].tiles[index].tile + ".bmp")
                }
-               else if(tC == 0 || tC == 1 || tC == 2 || tC == 3 || tC == 4 || tC == 5 || tC == 6 || tC == 7 || tC == 13 || tC == 14)
+               else if((map.layers[0].tiles[index].tile >= 0 && map.layers[0].tiles[index].tile <= 7) || map.layers[0].tiles[index].tile == 13 || map.layers[0].tiles[index].tile == 14)
                {
                     new Barrier(map.layers[0].tiles[index].x, map.layers[0].tiles[index].y, "tile" + map.layers[0].tiles[index].tile + ".bmp");
                }
