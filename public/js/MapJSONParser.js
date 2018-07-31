ceecc0979c9cdd64306415355fcfd41dd6b9853d
Tyler <neruelin@gmail.com>
2018-07-31 04:20:58 -0400

    asdfasdfasdfasdf

--- public/js/MapJSONParser.js
+++ public/js/MapJSONParser.js
@@ -17,23 +17,26 @@ function loadMap(map)
 {
      console.log(map.tileshigh);
      var newMap = new Map(map.tileshigh, map.tileshigh);
+     SG.Board = newMap;
      var mapData = new MapArr(map.tileshigh, map.tileshigh);
      var index = 0;
-     SG.Board = newMap;
-     console.log(SG);
 
-     for(var i = 0; i < map.tileshigh; i++)
+     for(var i = 0; i < mapData.rows; i++)
      {
-          for(var j = 0; j < map.tileshigh; j++)
+          for(var j = 0; j < mapData.columns; j++)
           {
-               console.log(map.layers[0].tiles[index].tile);
-               if((map.layers[0].tiles[index].tile >= 0 && map.layers[0].tiles[index].tile <= 7) || map.layers[0].tiles[index].tile == 13 || map.layers[0].tiles[index].tile == 14)
+               var tC = map.layers[0].tiles[index].tile;
+               console.log(tC)
+               if(tC == 8)
+               {
+                    new Terrain(map.layers[0].tiles[index].x, map.layers[0].tiles[index].y, "tile" + map.layers[0].tiles[index].tile + ".bmp")
+               }
+               else if(tC == 0 || tC == 1 || tC == 2 || tC == 3 || tC == 4 || tC == 5 || tC == 6 || tC == 7 || tC == 13 || tC == 14)
                {
                     new Barrier(map.layers[0].tiles[index].x, map.layers[0].tiles[index].y, "tile" + map.layers[0].tiles[index].tile + ".bmp");
                }
                else
-               {
-                    console.log("the obstacle")
+               {    
                     new Obstacle(map.layers[0].tiles[index].x, map.layers[0].tiles[index].y, "Obstacle", "tile" + map.layers[0].tiles[index].tile + ".bmp");
                }
                index++;
